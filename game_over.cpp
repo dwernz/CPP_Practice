0a98695bdbe2ceaa2bc65ad03fdb8b605ffdcf34
Daniel Wernz <dwernz@att.net>
2021-02-09 14:45:50 -0500

    Create game_over.cpp

---
+++ game_over.cpp
@@ -0,0 +1,12 @@
+// Game Over
+// First C++ program from "Beginning C++ Through Game Programming"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Game Over!" << endl;
+
+    return 0;
+}
